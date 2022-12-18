1e2e674a82f1661f25ddd0c347b1da5f25c19944
nanchacha <67902762+nanchacha@users.noreply.github.com>
2022-12-19 01:08:30 +0900

    Update HMCstatus.py
    change color for title

--- HMCstatus.py
+++ HMCstatus.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: #F0EDCC;'>HELP ME CHOOSE STATUS</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #F95700;'>HELP ME CHOOSE STATUS</h1>", unsafe_allow_html=True)
 
 cntrList = ['UK', 'ID', 'MY', 'EG', 'HU', 'LEVANT', 'ES', 'LEVANT_AR', 'HK_EN', 'HK', 'RO', 'SE', 'NO', 'DK', 'FI', 'AT', 'CH', 'VN', 'CH_FR', 'TW', 'IT', 'DE', 'PH', 'BR', 'CZ', 'SK', 
  'FR', 'N_AFRICA', 'PT', 'KZ_KZ', 'KZ_RU', 'AU', 'ZA', 'EE', 'LV', 'LT', 'CA', 'PY', 'AR', 'SI', 'HR', 'AE', 'TH', 'NL', 'BE', 'BE_FR', 'SG', 'CL', 'CO', 'MX', 'CA_FR', 'PL', 'UA', 'PK', 'PE', 'AE_AR', 'RU', 'UY']
