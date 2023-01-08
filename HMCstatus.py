2e596365e6706013ee4e9f0c2afd4b3cd939bc9b
차승원 <csw449@gmail.com>
2023-01-08 20:54:56 +0900

    웹사이트 정상 여부 체크로직 변경(기존 html status code 체크에서 html 요소 파악으로 변경)

--- HMCstatus.py
+++ HMCstatus.py
@@ -1,10 +1,13 @@
 import requests
 import streamlit as st
+from bs4 import BeautifulSoup
 
 st.markdown("<h1 style='text-align: center; color: #F95700;'>HELP ME CHOOSE STATUS</h1>", unsafe_allow_html=True)
 
 cntrList = ['UK', 'ID', 'MY', 'EG', 'HU', 'LEVANT', 'ES', 'LEVANT_AR', 'HK_EN', 'HK', 'RO', 'SE', 'NO', 'DK', 'FI', 'AT', 'CH', 'VN', 'CH_FR', 'TW', 'IT', 'DE', 'PH', 'BR', 'CZ', 'SK', 
  'FR', 'N_AFRICA', 'PT', 'KZ_KZ', 'KZ_RU', 'AU', 'ZA', 'EE', 'LV', 'LT', 'CA', 'PY', 'AR', 'SI', 'HR', 'AE', 'TH', 'NL', 'BE', 'BE_FR', 'SG', 'CL', 'CO', 'MX', 'CA_FR', 'PL', 'UA', 'PK', 'PE', 'AE_AR', 'RU', 'UY']
+
+# cntrList = ['UK']
 
 def statusYn(num):
     result = False
@@ -15,8 +18,22 @@ def statusYn(num):
 
 def getStatus(cntr):
     r = requests.get("https://www.samsung.com/{}/tvs/help-me-choose".format(cntr))
-    num = r.status_code
-    # print(cntr, num)
+    num = -1
+    html = r.content
+    soup = BeautifulSoup(html, 'html.parser')
+        
+    elem = soup.find('div', attrs={'class' : 'find-my-tv__header'})
+    try : 
+        title = elem.text.split('Depending')[0]
+        num = 200
+    except:
+        num = -1
+    # print(title)
+    # if 'Help Me Choose' in title:
+    #     # print('in!')
+    #     num = 200
+    # num = r.status_code
+    
     return num
 
 cnt = 1
