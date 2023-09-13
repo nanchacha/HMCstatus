e713c75959a5e69889f4d196a2e6394e8edba3fd
차승원 <csw449@gmail.com>
2023-09-13 22:48:03 +0900

    IQ_AR 사이트 추가

---
+++ test.py
@@ -0,0 +1,9 @@
+cntrList = ['GR', 'UK', 'ID', 'MY', 'EG', 'HU', 'LEVANT', 'ES', 'LEVANT_AR', 'HK_EN', 'HK', 'RO', 'SE', 'NO', 'DK', 'FI', 'AT', 'CH', 'VN', 'CH_FR', 
+            'TW', 'IT', 'DE', 'PH', 'BR', 'CZ', 'SK', 'FR', 'N_AFRICA', 'PT', 'KZ_KZ', 'KZ_RU', 'AU', 'ZA', 'EE', 'LV', 'LT', 'CA', 'PY', 'AR', 'SI', 
+            'HR', 'AE', 'TH', 'NL', 'BE', 'BE_FR', 'SG', 'CL', 'CO', 'MX', 'CA_FR', 'PL', 'UA', 'PK', 'PE', 'AE_AR', 'RU', 'UY', 'SA', 'SA_EN']
+
+
+print(len(cntrList))
+
+div = round(len(cntrList)/3)
+print(div)
