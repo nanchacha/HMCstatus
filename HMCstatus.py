1f486a27c66a508c411482df08976a4e0a5cc3e5
nanchacha <67902762+nanchacha@users.noreply.github.com>
2024-01-30 12:25:33 +0900

    Update HMCstatus.py
    인도사이트 추가

--- HMCstatus.py
+++ HMCstatus.py
@@ -7,7 +7,7 @@ st.markdown("<h1 style='text-align: center; color: #F95700;'>HELP ME CHOOSE STAT
 
 cntrList = ['IQ_AR', 'GR', 'UK', 'ID', 'MY', 'EG', 'HU', 'LEVANT', 'ES', 'LEVANT_AR', 'HK_EN', 'HK', 'RO', 'SE', 'NO', 'DK', 'FI', 'AT', 'CH', 'VN', 'CH_FR', 
             'TW', 'IT', 'DE', 'PH', 'BR', 'CZ', 'SK', 'FR', 'N_AFRICA', 'PT', 'KZ_KZ', 'KZ_RU', 'AU', 'ZA', 'EE', 'LV', 'LT', 'CA', 'PY', 'AR', 'SI', 
-            'HR', 'AE', 'TH', 'NL', 'BE', 'BE_FR', 'SG', 'CL', 'CO', 'MX', 'CA_FR', 'PL', 'UA', 'PK', 'PE', 'AE_AR', 'RU', 'UY', 'SA', 'SA_EN', 'IL', 'NZ']
+            'HR', 'AE', 'TH', 'NL', 'BE', 'BE_FR', 'SG', 'CL', 'CO', 'MX', 'CA_FR', 'PL', 'UA', 'PK', 'PE', 'AE_AR', 'RU', 'UY', 'SA', 'SA_EN', 'IL', 'NZ', 'IN']
 
 # cntrList = ['GR']
 
